7dff1c0b936c0267335e30fecdeefb8d09b28052
SpikyShade <103762012+spikyshade@users.noreply.github.com>
2023-01-04 10:58:23 +0000

    Just work dude

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template
 from .views import views
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRECT_KEY'] = 'THis isi myldjfdkl'
+app = Flask(__name__)
 
-    app.register_blueprint(views, url_prefix='/')
+app.config['SECRECT_KEY'] = 'THis isi myldjfdkl'
 
-    @app.errorhandler(404)
-    def not_found(e):
-        return render_template('404.html')
-    
-    return app
+app.register_blueprint(views, url_prefix='/')
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
